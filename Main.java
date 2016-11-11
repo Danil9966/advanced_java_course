2695352c0a2b267b0c5e8e4b4acc5149f005e1e5
Danil9966 <danilikus17@gmail.com>
2016-11-11 17:49:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package pokemonG;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pokemon pic=new Pikachu();
+        Pokemon sq=new Squirtle();
+        //fight
+	// write your code here
+    }
+}
